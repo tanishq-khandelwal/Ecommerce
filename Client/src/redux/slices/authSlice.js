82758843d17221045c6b752f24085674a077f040
tanishq-khandelwal <tanishqkhandelwal2019@gmail.com>
2025-02-09 10:42:31 +0530

    Added Render backend

--- Client/src/redux/slices/authSlice.js
+++ Client/src/redux/slices/authSlice.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/api/v1/user",
+    baseUrl: "https://ecommerce-pjdh.onrender.com/api/v1/user",
     credentials: "include", 
   }),
   endpoints: (builder) => ({
